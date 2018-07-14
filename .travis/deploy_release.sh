35a128b88a3e60274d176ccba40382cf22fda0ec
Maxim Biro <nurupo.contributions@gmail.com>
2018-07-14 16:26:44 -0400

    Test

--- .travis/deploy_release.sh
+++ .travis/deploy_release.sh
@@ -12,12 +12,10 @@ python ./ci_release_publisher.py publish -h
 python ./ci_release_publisher.py collect ./deploy
 python ./ci_release_publisher.py cleanup
 python ./ci_release_publisher.py publish --latest-release \
-                                         --latest-release-draft \
                                          --latest-release-prerelease \
                                          --numbered-release \
                                          --numbered-release-keep-count 3 \
                                          --numbered-release-keep-time "$((24 * 60 * 60))" \
-                                         --numbered-release-draft \
                                          --numbered-release-prerelease \
                                          --tag-release \
                                          --tag-release-draft \
