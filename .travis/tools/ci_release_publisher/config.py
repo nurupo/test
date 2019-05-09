497cbc4a8ef66a141b7cfbecdad2004a46c2397d
Maxim Biro <nurupo.contributions@gmail.com>
2019-05-08 21:07:46 -0400

    Test

--- .travis/tools/ci_release_publisher/config.py
+++ .travis/tools/ci_release_publisher/config.py
@@ -10,4 +10,4 @@ tag_prefix_tmp = '_'
 timeout = 15
 
 def retries():
-    return Retry(total=7, backoff_factor=0.1, status_forcelist=[403, 500, 502, 503, 504])
+    return Retry(total=7, backoff_factor=0.1, status_forcelist=[403, 500, 502, 503, 504], method_whitelist=list(Retry.DEFAULT_METHOD_WHITELIST)+['POST'])
